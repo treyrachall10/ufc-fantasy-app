724f90be8c06ef8331d43ede8617995f1ad4fbe0
Trey-Rachall <mathew.rachall@gmail.com>
2025-12-20 12:05:39 -0600

    updated fighter and fighterwithcareerstats interfaces

--- frontend/ufc-fantasy-frontend/src/types/types.ts
+++ frontend/ufc-fantasy-frontend/src/types/types.ts
@@ -14,54 +14,70 @@ export interface Fighter {
     reach: number | null,
     dob: string | null,
     record: {
-        wins: number,
-        losses: number,
+        wins: {
+            total: number,
+            ko_tko_wins: number,
+            tko_doctor_stoppage_wins: number,
+            submission_wins: number,
+            unanimous_decision_wins: number,
+            split_decision_wins: number,
+            majority_decision_wins: number,
+            dq_wins: number,
+        },
+        losses: {
+            total: number,
+            ko_tko_losses: number,
+            tko_doctor_stoppage_losses: number,
+            submission_losses: number,
+            unanimous_decision_losses: number,
+            split_decision_losses: number,
+            majority_decision_losses: number,
+            dq_losses: number,
+        },
         draws: number
     } | null,
 }
 
-export interface FighterWithCareerStats{
-    fighter: Fighter,
+export interface FighterWithCareerStats {
+    fighter: Fighter;
+  
     total_fights: number;
-    wins: number;
-    losses: number;
-    draws: number;
-
-    ko_tko: number;
-    tko_doctor_stoppages: number;
-    submissions: number;
-
-    unanimous_decisions: number;
-    split_decisions: number;
-    majority_decisions: number;
-    dq: number;
-
+    total_fight_time: number;
+  
+    // Fighter striking
     sig_str_landed: number;
     sig_str_attempted: number;
     total_str_landed: number;
     total_str_attempted: number;
-
-    td_landed: number;
-    td_attempted: number;
-
-    sub_att: number;
-    ctrl_time: number;
-    reversals: number;
-
+  
+    distance_str_landed: number;
+    distance_str_attempted: number;
     head_str_landed: number;
     head_str_attempted: number;
     body_str_landed: number;
     body_str_attempted: number;
     leg_str_landed: number;
     leg_str_attempted: number;
-
-    distance_str_landed: number;
-    distance_str_attempted: number;
     clinch_str_landed: number;
     clinch_str_attempted: number;
     ground_str_landed: number;
     ground_str_attempted: number;
-}
+  
+    // Fighter grappling
+    td_landed: number;
+    td_attempted: number;
+    sub_att: number;
+    reversals: number;
+    ctrl_time: number;
+  
+    // Opponent
+    opp_sig_str_landed: number;
+    opp_sig_str_attempted: number;
+  
+    opp_td_landed: number;
+    opp_td_attempted: number;
+    opp_ctrl_time: number;
+  }
 
 export interface Event {
     event_id: number,
