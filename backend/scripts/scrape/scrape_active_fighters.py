84c631028a4ac1f9ea1780cd1dcb3918c0404844
Trey-Rachall <mathew.rachall@gmail.com>
2026-02-20 21:07:25 -0600

    Uppercased active_fighter_page_url and headers by convention

--- backend/scripts/scrape/scrape_active_fighters.py
+++ backend/scripts/scrape/scrape_active_fighters.py
@@ -2,7 +2,7 @@ from bs4 import BeautifulSoup
 import requests
 import json
 import pandas as pd
-active_fighter_page_url = "https://www.ufc.com/athletes/all?filters%5B0%5D=status%3A23"
+ACTIVE_FIGHTER_URL = "https://www.ufc.com/athletes/all?filters%5B0%5D=status%3A23"
 
 url_params = {
     "view_name": "all_athletes",
@@ -17,17 +17,16 @@ url_params = {
     "ajax_page_state[theme_token]": ""
 }
 
-headers = {
+HEADERS = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
 }
-
 
 def scrape_active_fighters():
     """
     Scrapes the UFC website for active fighters and returns a set of their normalized names.
     """
     session = requests.Session()
-    response = session.get(active_fighter_page_url)
+    response = session.get(ACTIVE_FIGHTER_URL, headers=HEADERS)
     response.raise_for_status()
     
     # Parse response to extract required parameters
@@ -50,7 +49,7 @@ def scrape_active_fighters():
     # Loop through pages until no more fighters are found
     while True:
         url_params["page"] = page
-        response = session.get(active_fighter_page_url, params=url_params, headers=headers)
+        response = session.get(ACTIVE_FIGHTER_URL, params=url_params, headers=HEADERS)
         soup = BeautifulSoup(response.text, "html.parser")
         
         fighters = soup.select(".c-listing-athlete__name")
