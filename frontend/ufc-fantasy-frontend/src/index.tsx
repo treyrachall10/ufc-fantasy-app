f3a81607d522f80fe540c6a4391b111bfa288bf0
Trey-Rachall <mathew.rachall@gmail.com>
2026-02-22 14:01:20 -0600

    Import Auth0Provider, wrap application

--- frontend/ufc-fantasy-frontend/src/index.tsx
+++ frontend/ufc-fantasy-frontend/src/index.tsx
@@ -3,14 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Auth0Provider } from '@auth0/auth0-react';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Auth0Provider
+    domain="dev-kxp1v6beff35mbat.us.auth0.com"
+    clientId="30r14jTMf8eR5QYcyEbSaUJw5QSWd01B"
+  >
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Auth0Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
