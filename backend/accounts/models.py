fce2491548e41e0b5f669985761528b91a529db4
Trey-Rachall <mathew.rachall@gmail.com>
2026-02-26 10:36:29 -0600

    Update User model to include username, auth0id, and profile_complete

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -3,3 +3,6 @@ from django.db import models
 
 class User(AbstractUser):
     email = models.EmailField(unique=True)
+    auth0_id = models.CharField(max_length=255, unique=True)
+    username = models.CharField(max_length=255, unique=True)
+    profile_complete = models.BooleanField(default=False)
