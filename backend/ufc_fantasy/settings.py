0be4e9a49848cfab55d811f9462183982c38ca3b
Trey-Rachall <mathew.rachall@gmail.com>
2026-03-03 08:18:47 -0600

    Implement .env file for supabase database integration and remove db service

--- backend/ufc_fantasy/settings.py
+++ backend/ufc_fantasy/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -96,11 +97,14 @@ WSGI_APPLICATION = 'ufc_fantasy.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'db',
-        'PORT': 5432
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
+        "OPTIONS": {
+            "sslmode": "require",
+        },
     }
 }
 
