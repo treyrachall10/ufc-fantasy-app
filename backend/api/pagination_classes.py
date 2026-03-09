271220c2796bc1a719924c1d41ee223d932a2aad
Trey-Rachall <mathew.rachall@gmail.com>
2026-03-08 20:30:11 -0500

    Removed page_size_query_param

--- backend/api/pagination_classes.py
+++ backend/api/pagination_classes.py
@@ -3,5 +3,4 @@ from rest_framework.pagination import PageNumberPagination
 
 class FighterListPagination(PageNumberPagination):
 	page_size = 100
-	page_size_query_param = "size"
 	max_page_size = 100
