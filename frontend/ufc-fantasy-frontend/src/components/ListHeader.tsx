8c4a7e0a17ccff4ff20ad491ba19421dd43e0d4c
Trey-Rachall <mathew.rachall@gmail.com>
2026-04-03 13:26:39 -0500

    Frontend fighter search capability

--- frontend/ufc-fantasy-frontend/src/components/ListHeader.tsx
+++ frontend/ufc-fantasy-frontend/src/components/ListHeader.tsx
@@ -1,16 +1,33 @@
 import { TextField } from '@mui/material';
 import Stack from '@mui/material/Stack';
+import { KeyboardEvent } from 'react';
 
 interface ListHeaderProps {
     title: string;
     searchBarLabel: string;
+    searchValue?: string;
+    onSearchChange?: (value: string) => void;
+    onSearchEnter?: () => void;
 }
 
 export default function ListHeader(props: ListHeaderProps) {
+    const handleKeyDown = (event: KeyboardEvent<HTMLInputElement>) => {
+        if (event.key === 'Enter') {
+            props.onSearchEnter?.();
+        }
+    };
+
     return (
         <Stack direction="row" justifyContent={'space-between'} alignItems={'center'}>
             <h2>{props.title}</h2>
-            <TextField id="outlined-basic" label={props.searchBarLabel} variant="outlined"></TextField>
+            <TextField
+                id="outlined-basic"
+                label={props.searchBarLabel}
+                variant="outlined"
+                value={props.searchValue ?? ''}
+                onChange={(event) => props.onSearchChange?.(event.target.value)}
+                onKeyDown={handleKeyDown}
+            ></TextField>
         </Stack>
     )
 }
