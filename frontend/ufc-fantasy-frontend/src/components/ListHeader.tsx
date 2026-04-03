0311849749213971cc9b6693d192bee53dcecb26
Trey-Rachall <mathew.rachall@gmail.com>
2026-04-03 13:47:08 -0500

    Created and Styled search icon button

--- frontend/ufc-fantasy-frontend/src/components/ListHeader.tsx
+++ frontend/ufc-fantasy-frontend/src/components/ListHeader.tsx
@@ -1,5 +1,8 @@
 import { TextField } from '@mui/material';
+import IconButton from '@mui/material/IconButton';
+import InputAdornment from '@mui/material/InputAdornment';
 import Stack from '@mui/material/Stack';
+import SearchIcon from '@mui/icons-material/Search';
 import { KeyboardEvent } from 'react';
 
 interface ListHeaderProps {
@@ -27,6 +30,23 @@ export default function ListHeader(props: ListHeaderProps) {
                 value={props.searchValue ?? ''}
                 onChange={(event) => props.onSearchChange?.(event.target.value)}
                 onKeyDown={handleKeyDown}
+                sx={{bgcolor: 'black'}}
+                slotProps={{
+                    input: {
+                        endAdornment: (
+                            <InputAdornment position="end" sx={{bgcolor: 'black'}}>
+                                <IconButton
+                                    aria-label="search fighters"
+                                    onClick={() => props.onSearchEnter?.()}
+                                    edge="end"
+                                    sx={{bgcolor: 'black'}}
+                                >
+                                    <SearchIcon sx={{ color: 'common.white' }} />
+                                </IconButton>
+                            </InputAdornment>
+                        ),
+                    },
+                }}
             ></TextField>
         </Stack>
     )
