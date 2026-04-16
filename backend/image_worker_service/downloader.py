c38e73ba8e21dc70ae07d05e64c4172311aeda16
Trey-Rachall <mathew.rachall@gmail.com>
2026-04-16 08:45:46 -0500

    Download image files

--- backend/image_worker_service/downloader.py
+++ backend/image_worker_service/downloader.py
@@ -1,3 +1,6 @@
+import requests
+from PIL import Image
+
 from backend.shared.job_queue import get_job
 
 def consume_jobs():
@@ -5,6 +8,7 @@ def consume_jobs():
         Continuously consumes jobs from the worker queue and processes them. 
     '''
     while True:
+        print("Waiting for job...")
         job = get_job()
         # Process the job
         download_image(job)
@@ -13,3 +17,12 @@ def download_image(job):
     '''
         Downloads image from given url in job, sends image file to uploader
     '''
+    url = job['img_url']
+    fighter_id = job['fighter_id']
+    response = requests.get(url)
+    response.raise_for_status() # Check if request was successful
+
+    # Save image to a file and send to uploader
+    with open(f'{fighter_id}.jpg', 'wb') as f:
+        for chunk in response.iter_content(1024):
+            pass
