f35605f0e5bb1603b31bbd444c8ac161f959ba18
Trey-Rachall <mathew.rachall@gmail.com>
2026-04-16 21:50:40 -0500

    New permission

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -8,3 +8,11 @@ class IsAthleteImageService(BasePermission):
         key = header.split()[1] # Extract API key from header (assuming format "Api-Key <key>")
         api_key = APIKey.objects.get_from_key(key) # Get APIKey object from key
         return api_key is not None and api_key.name == "athlete_image_service" # Check name of api to ensure it'c correct service
+
+# Custom permission for uploader service
+class IsUploaderService(BasePermission):
+    def has_permission(self, request, view):
+        header = request.META.get("HTTP_AUTHORIZATION", "") # Get authorization header
+        key = header.split()[1] # Extract API key from header (assuming format "Api-Key <key>")
+        api_key = APIKey.objects.get_from_key(key) # Get APIKey object from key
+        return api_key is not None and api_key.name == "uploader_service" # Check name of api to ensure it's the correct service
