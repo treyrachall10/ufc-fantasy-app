6736e35b05801fda7cf179da2a8ad2a9124a8997
Trey-Rachall <mathew.rachall@gmail.com>
2026-04-17 21:54:34 -0500

    New update athlete images command

---
+++ backend/fantasy/management/commands/update_athlete_images.py
@@ -0,0 +1,15 @@
+import threading
+from django.core.management.base import BaseCommand
+from image_worker_service.downloader import consume_jobs
+from athlete_image_service.scrape_fighter_images import scrape_fighter_images_df
+
+class Command(BaseCommand):
+    """
+        -   Creates the custom command to update athlete images
+    """
+    help = "Updates athlete images in the database"
+
+    def handle(self, *args, **options):
+        thread = threading.Thread(target=consume_jobs, daemon=False) # Start worker thread to consume jobs from queue
+        thread.start()
+        scrape_fighter_images_df()
