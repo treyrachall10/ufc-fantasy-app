a88c465c3fd24ee0fe5515405e7be7034d43d0d7
Trey-Rachall <mathew.rachall@gmail.com>
2026-04-17 21:54:09 -0500

    Include fighter_id url param, fix wrong library issue

--- backend/image_worker_service/uploader.py
+++ backend/image_worker_service/uploader.py
@@ -12,7 +12,7 @@ BASE_API_URL = config['base_api_url']
 BUCKET_NAME = os.environ.get("SUPABASE_FIGHTER_IMAGE_BUCKET")
 uploader_service_key = os.getenv("UPLOADER_SERVICE_KEY")
 
-def upload_img(file_path, file, content_type):
+def upload_img(file_path, file, content_type, fighter_id):
     '''
         Uploads image file to Supabase storage bucket.
     '''
@@ -28,20 +28,28 @@ def upload_img(file_path, file, content_type):
             
             )
         
-        print(f"Image uploaded successfully to {BUCKET_NAME}/{file_path}")
+        update_fighter_img_url(fighter_id, file_path)
     except Exception as e:
         print(f"Error uploading image to {BUCKET_NAME}/{file_path}: {e}")
 
 def update_fighter_img_url(fighter_id, img_url):
     '''
         Calls api to update fighter image url.
+        Parameters: 
+            fighter_id (int): ID of fighter to update image URL for
+            img_url (str): URL of fighter image to update in database
     '''
     try:
-        res = request.patch(
+        res = requests.patch(
             BASE_API_URL + f"{fighter_id}/SetFighterImage",
             headers={'Authorization': f'Api-Key {uploader_service_key}'},
             json={'img_url': img_url}
         )
+
+        if not res.ok:
+            print(f"Failed to update fighter image URL for fighter ID {fighter_id}. Response: {res.text}")
+            
         res.raise_for_status()
+        print(f"Image uploaded successfully to {BUCKET_NAME}/{img_url}") 
     except Exception as e:
         print(f"Error updating fighter image URL for fighter ID {fighter_id}: {e}")
