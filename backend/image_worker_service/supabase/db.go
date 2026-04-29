dae25c99324a92f14c577581e20a0242cfd866bb
Trey-Rachall <mathew.rachall@gmail.com>
2026-04-28 20:51:39 -0500

    Create db functions

--- backend/image_worker_service/supabase/db.go
+++ backend/image_worker_service/supabase/db.go
@@ -19,3 +19,53 @@ func NewPostgresClient() *pgx.Conn {
 	log.Println("Connected to database")
 	return conn
 }
+
+func bulkUpdateImageJobs(conn *pgx.Conn, jobs []types.ImageJob) error {
+
+	args := generateArgsList(jobs)                // Generate the arguments list for the query
+	sqlStrings := generateBulkSQLSring(len(jobs)) // Generate the SQL strings for the query
+
+	// Build the query
+	query := fmt.Sprintf(`
+		UPDATE image_job as ij
+		SET 
+			status = 'COMPLETED',
+			supabase_path = 'fighter-images/' || data.fighter_id || '/' || data.supabase_path,
+			updated_at = NOW(),
+			completed_at = NOW()
+		FROM (
+			VALUES %s
+		) as data(id, fighter_id,supabase_path)
+		WHERE ij.id = data.id)
+	`, sqlStrings)
+
+	_, err := conn.Exec(context.Background(), query, args...)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func generateBulkSQLSring(length int) string {
+	paramIndex := 1
+	s := ""
+	// Loop through the length and generate the SQL strings
+	for i := 0; i < length; i++ {
+		s += fmt.Sprintf("($%d, $%d, $%d, $%d)", paramIndex, paramIndex+1, paramIndex+2, paramIndex+3) // Format the values for the query
+		paramIndex += 4
+		// Add comma between values
+		if i < length-1 {
+			s += ", "
+		}
+	}
+	return s
+}
+
+func generateArgsList(jobs []types.ImageJob) []interface{} {
+	// Generate the arguments list for the query
+	args := make([]interface{}, 0)
+	for _, job := range jobs {
+		args = append(args, job.ID, job.FighterID, job.SupabasePath)
+	}
+	return args
+}
