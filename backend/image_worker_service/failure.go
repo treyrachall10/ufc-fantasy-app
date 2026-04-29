2630f757018324c3369c3904bb39d83b068117d2
Trey-Rachall <mathew.rachall@gmail.com>
2026-04-29 08:58:32 -0500

    Handle dead messages

--- backend/image_worker_service/failure.go
+++ backend/image_worker_service/failure.go
@@ -13,12 +13,22 @@ func handleFailedJob(job *types.ImageJob, errMsg string) {
 
 	fmt.Printf("Job failed for %s: %s\n", job.NormalizedName, errMsg)
 
+	// If the retry count is less than 3, increment the retry count, update the job in the database and nack the message
 	if job.RetryCount < 3 {
 		job.RetryCount++
 		if job.Msg != nil {
-			job.Msg.Nack()
+			job.Msg.Nack() // Nack the message to put it back in the subscription
 			return
 		}
 		fmt.Printf("Cannot nack job %d: message is nil\n", job.ID)
+	}
+
+	// If the retry count is greater than or equal to 3, update the job in the database and ack the message
+	if job.RetryCount >= 3 {
+		if job.Msg != nil {
+			job.Msg.Ack() // Ack the message to remove it from the subscription
+			return
+		}
+		fmt.Printf("Cannot ack job %d: message is nil\n", job.ID)
 	}
 }
